e47ec9213def45840647341e091f0a3fe6ad805f
Marsel <bard1812@gmail.com>
2022-10-23 16:54:55 +0300

    create user controller

---
+++ src/main/java/ru/min/controller/UserController.java
@@ -0,0 +1,46 @@
+package ru.min.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.min.entity.User;
+import ru.min.repository.UserRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping(path = "/{username}")
+    public Optional<User> find(@PathVariable("username") String username){
+        return userRepository.findById(username);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public User create(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping(path = "/{username}")
+    public void delete(@PathVariable("username") String username){
+        userRepository.deleteById(username);
+    }
+
+    @PutMapping(path = "/{username}")
+    public User update(@PathVariable("username") String username, @RequestBody User user) throws Exception {
+        if (userRepository.existsById(username)){
+            user.setUsername(username);
+            return userRepository.save(user);
+        }else {
+            throw new Exception("Bad Request!!!");
+        }
+    }
+}
--- src/main/java/ru/min/repository/UserRepository.java
+++ src/main/java/ru/min/repository/UserRepository.java
@@ -2,7 +2,9 @@ package ru.min.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.min.entity.User;
 
+@RestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, String> {
 }
