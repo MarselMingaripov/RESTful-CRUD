121b9a7e4cc7698c7e1996cf543b509ec16dfcd1
MarselMingaripov <bard1812@gmail.com>
2022-12-16 22:09:29 +0300

    исправил тест для метода findUSerByUsername интерфейса UserRepository

--- src/test/java/repository/UserRepositoryTest.java
+++ src/test/java/repository/UserRepositoryTest.java
@@ -2,17 +2,12 @@ package repository;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.min.entity.User;
 import ru.min.repository.UserRepository;
-
 import javax.persistence.EntityManager;
-import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
-
-
+import static org.mockito.BDDMockito.given;
 
 
 class UserRepositoryTest {
@@ -24,22 +19,27 @@ class UserRepositoryTest {
 
 
     @Test
-    void whenFindByUsername_thenReturnUser() {
+    void whenFindUserByUsername_thenReturnUser() {
         ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-        User user = new User();//mock(User.class);
+        User user = new User();
         user.setUsername(USERNAME);
         user.setEmail("emailTest@mail.com");
         user.setPassword("123456Qw");
+        given(userRepository.findUserByUsername(USERNAME)).willReturn(user);
         entityManager.persist(user);
         entityManager.flush();
-
+        System.out.println(user);
         Mockito.verify(entityManager).persist(captor.capture());
 
         User actual = captor.getValue();
-        System.out.println(actual);
+        User found = userRepository.findUserByUsername(USERNAME);
+        Mockito.verify(userRepository).findUserByUsername(USERNAME);
+
+        //System.out.println(actual);
+        System.out.println(found);
 
         assertNotNull(actual);
-        assertEquals(user, actual);
+        assertEquals(actual, found);
     }
 
 //    @Test
