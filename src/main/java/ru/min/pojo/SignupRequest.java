aeda708ed20e242c0fb3b8835d051a251f46f030
MarselMingaripov <bard1812@gmail.com>
2022-12-17 19:13:46 +0300

    слил все ветки, проверил работоспособность

--- src/main/java/ru/min/pojo/SignupRequest.java
+++ src/main/java/ru/min/pojo/SignupRequest.java
@@ -2,7 +2,6 @@ package ru.min.pojo;
 
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.util.Set;
